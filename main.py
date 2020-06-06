f3d9387e81a234b02471505ee4ffd863e5b7d0ed
Yuliya <anick2704@mail.ru>
2020-06-06 14:43:59 +0300

    scrollbar added

--- main.py
+++ main.py
@@ -40,8 +40,15 @@ root.geometry('+{}+{}'.format(w, h) )
 
 l = Label(bg = '#FFFFCC')
 l.grid(row = 0, columnspan=2)
-text = Text(bg = '#CCCCCC', fg = '#006600')
-text.grid(row =1, columnspan=2, sticky=N+W+E)
+
+fr = Frame()
+fr.grid(row =1, columnspan=2, sticky=N+W+E)
+text = Text(fr, bg = '#CCCCCC', fg = '#006600', width=50, height=20, wrap=NONE)
+text.pack(side=LEFT)
+scroll = Scrollbar(fr, command=text.yview)
+scroll.pack(fill=Y, side=LEFT)
+text.config(yscrollcommand=scroll.set)
+
 b1 = Button(text="Open", command=getFile, width=8, height=2, fg = "#123FEA")
 b1.grid(row=2, sticky=E)
 b2 = Button(text="Save", command=saveFile, width=8, height=2, fg = "#123FEA")
